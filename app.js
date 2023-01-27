a27eaf7ffc908443f8fd2df3109010e55b26ca84
Thomas Fuhriman <seniorswanky@icloud.com>
2023-01-27 16:18:23 -0700

    commit

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 let clickMaster = {
     click: 0,
-    rest: 0,
-    yawn: 0
+    clickValue: 100,
 }
-let clickCash = {
-    clickValue: 100
-}
+// let clickCash = {
+//     clickValue: 100
+// }
 let clickAuto = {
     autoFeed: 50,
     autoCatch: 100
 }
-let upgradeCost = {
-    yawn: 2,
-    rest: 5
-}
+// let upgradeCost = {
+//     yawn: 2,
+//     rest: 5
+// }
 let clickVal = {
     rest: 0,
     yawn: 0
 }
+
+let clickUpgrades = [
+    {
+    name: 'yawn',
+    price: 3,
+    quantity: 0,
+    multiplier: 1.2
+    },
+    {
+        name: 'rest',
+        price: 6,
+        quantity: 0,
+        multiplier: 1.8
+    }
+];
 
 
 function mainClick(){
@@ -26,67 +40,72 @@ function mainClick(){
 
 
     clicks.innerText = clickMaster.click++
-    clickValue.innerText = clickCash.clickValue++ 
+    clickValue.innerText = clickMaster.clickValue++ 
     console.log(clickMaster.click)
 
-    clickUp()
+    // clickUp()
 }
 
+function buyUpgrades(upgradeName){
+    let clickValue = document.getElementById('clickValue')
+    let yawnUp = document.getElementById('yawnUp')
 
-
-function clickUp(){
-    let value = document.getElementById('clickValue')
-
-
-    if (clickVal.yawn > 0) {
-        clickVal.yawn * 1.5
-    }else{
-        console.log('no yawn upgrades to click')
-    }
+    const purchasedUpgrade = clickUpgrades.find(p => p.name == upgradeName)
     
-    if (clickVal.rest > 0) {
-        clickVal.rest * 1.5
-    } else{
-        console.log('no rest upgrades to click')
+    console.log(purchasedUpgrade);
+    if (clickMaster.clickValue >= purchasedUpgrade.price) {
+        clickMaster.clickValue -= purchasedUpgrade.price
     }
-
-
-
+    purchasedUpgrade.quantity += 1
+    yawnUp.innerText = purchasedUpgrade.quantity
+    clickValue.innerText = clickMaster.clickValue
 }
 
-function yawnUpgrade(){
+function addUpgrade(){
     let yawnUp = document.getElementById('yawnUp')
     let cValue = document.getElementById('clickValue')
-
-    if (clickCash.clickValue >= upgradeCost.yawn) {
-        clickVal.yawn++
-        clickCash.clickValue -= upgradeCost.yawn
-    }else(
-        console.log("not enough!")
-    )
-
-    console.log(clickCash.clickValue)
-    yawnUp.innerText = clickVal.yawn
-    cValue.innerText = clickCash.clickValue
-    console.log()
-    clickUp()
+    yawnUp.innerText = yawnUpgrade.quantity
+    cValue.innerText = clickMaster.clickValue
 }
 
-function restUpgrade(){
-    let yawnUp = document.getElementById('restUp')
-    let cValue = document.getElementById('clickValue')
 
-    if (clickCash.clickValue >= upgradeCost.rest) {
-        clickVal.rest++
-        clickCash.clickValue -= upgradeCost.rest
-    }else(
-        console.log("not enough!")
-    )
+// function clickUp(){
+//     let value = document.getElementById('clickValue')
 
-    console.log(clickCash.clickValue)
-    yawnUp.innerText = clickVal.rest
-    cValue.innerText = clickCash.clickValue
-    console.log()
-    clickUp()
-}
+
+//     if (clickVal.yawn > 0) {
+//         clickVal.yawn * 1.5
+//     }else{
+//         console.log('no yawn upgrades to click')
+//     }
+    
+//     if (clickVal.rest > 0) {
+//         clickVal.rest * 1.5
+//     } else{
+//         console.log('no rest upgrades to click')
+//     }
+// }
+
+// function yawnUpgrade(){
+
+//     let yawnUp = document.getElementById('yawnUp')
+//     let cValue = document.getElementById('clickValue')
+
+//     if (clickMaster.clickValue >= clickUpgrade.price) {
+//         yawnClick.quantity++
+//         clickMaster.clickValue -= clickUpgrade.price
+//     }else(
+//         console.log("not enough!")
+//     )
+
+//     console.log(clickCash.clickValue)
+//     yawnUp.innerText = yawnUpgrade.quantity
+//     cValue.innerText = clickMaster.clickValue
+//     console.log()
+//     // clickUp()
+// }
+
+// function restUpgrade(){
+    
+// }
 
