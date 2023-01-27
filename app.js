95b215b1f6460b00b473d083f9c53d6865af56ad
Thomas Fuhriman <seniorswanky@icloud.com>
2023-01-27 12:07:57 -0700

    c

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
-let click = 0
+let clickMaster = {
+    click: 1,
+    rest: 0,
+    yawn: 0
+}
+let clickCash = {
+    clickValue: 100
+}
+let clickAuto = {
+    autoFeed: 50,
+    autoCatch: 100
+}
+let upgradeCost = {
+    rest: 5,
+    yawn: 50
+}
+let clickVal = {
+    rest: 0,
+    yawn: 0
+}
 
 
 function mainClick(){
     let clicks = document.getElementById('clickCount')
 
-    
+    clicks.innerText = clickMaster.click++
+
+    console.log(clickMaster.click)
+
+    clickValue()
+}
+
+
+
+function clickValue(){
+    let value = document.getElementById('clickValue')
+    let clickValue = 0
+
+    clickMaster.click * clickMaster.rest
+
+
+}
+
+function yawnUpgrade(){
+    let yawnUpgrade = document.getElementById('yawnBtn')
+    let yawnUp = document.getElementById('yawnUp')
+
+    if (clickCash.clickValue >= upgradeCost.yawn) {
+        clickVal.yawn++
+        
+
+    }
+    yawnUpgrade.innerHTML = 
+    console.log()
 }
